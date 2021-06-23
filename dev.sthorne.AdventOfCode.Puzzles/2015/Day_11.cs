5b1927b0844e311cfe3de332f342b33ec6007969
steventhorne <steven.thorne91@gmail.com>
2021-06-22 21:54:30 -0400

    Stubbed day 11 puzzles.

---
+++ dev.sthorne.AdventOfCode.Puzzles/2015/Day_11.cs
@@ -0,0 +1,34 @@
+﻿using dev.sthorne.AdventOfCode.Puzzles.Day;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace dev.sthorne.AdventOfCode.Puzzles._2015
+{
+	[PuzzleDate(2015, 11)]
+	public class Day_11 : BaseDay
+	{
+		public Day_11(ILogger<Day_11> logger)
+			: base(logger)
+		{
+			Puzzles.Add(Puzzle_01);
+			Puzzles.Add(Puzzle_02);
+		}
+
+		protected override Task ProcessInput()
+		{
+			return Task.CompletedTask;
+		}
+
+		private Task<object> Puzzle_01()
+		{
+			return Task.FromResult((object)null);
+		}
+
+		private Task<object> Puzzle_02()
+		{
+			return Task.FromResult((object)null);
+		}
+	}
+}
